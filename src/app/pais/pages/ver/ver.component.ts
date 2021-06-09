34df723b3628608a189bdcf653f1b61346594fc3
Alfonso Carmona <alfonso.carmona.94@outlook.com>
2021-06-08 23:12:20 -0500

    Starting stucture

---
+++ src/app/pais/pages/ver/ver.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver',
+  templateUrl: './ver.component.html',
+  styles: [
+  ]
+})
+export class VerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
